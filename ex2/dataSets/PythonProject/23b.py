58c371dcd725e945e10d6f000fc0889319ef6264
David <davidgroemling@gmail.com>
2016-06-07 23:22:58 +0200

    comparison of knn and kde started

--- ex2/dataSets/PythonProject/23b.py
+++ ex2/dataSets/PythonProject/23b.py
@@ -23,6 +23,7 @@ def calculateDensity(evalat, sample, sigma):
 
 # Load the dataset
 samples = np.array([np.loadtxt(fname='../nonParamTrain.txt')]).transpose()
+testset = np.array([np.loadtxt(fname='../nonParamTest.txt')]).transpose()
 x = np.array([np.arange(-4, 8, 0.1)])
 
 for sigma in [0.03, 0.2, 0.8]:
@@ -31,6 +32,11 @@ for sigma in [0.03, 0.2, 0.8]:
     # calculate likelihood
     likelihood = calculateDensity(samples.transpose(), samples, sigma)
     likelihood = np.sum(np.log(likelihood))
+
+    # Calculate likelihood of Testset
+    likelihood_t = calculateDensity(testset.transpose(), samples, sigma)
+    likelihood_t = np.sum(np.log(likelihood_t))
+    print likelihood_t
 
     # plot result
     plt.plot(x[0, ...], distribution, label="KDE with sigma {} and likelihood {}".format(sigma, likelihood))
--- ex2/dataSets/PythonProject/23c.py
+++ ex2/dataSets/PythonProject/23c.py
@@ -22,6 +22,7 @@ def calculateknn(evalat, samples, K):
 
 # Load the dataset
 samples = np.array([np.loadtxt(fname='../nonParamTrain.txt')]).transpose()
+testset = np.array([np.loadtxt(fname='../nonParamTest.txt')]).transpose()
 x = np.array([np.arange(-4, 8, 0.1)])
 
 for K in [2, 8, 35]:
@@ -31,10 +32,14 @@ for K in [2, 8, 35]:
     likelihood = calculateknn(samples.transpose(), samples, K)
     likelihood = np.sum(np.log(likelihood))
 
+    # Calculate likelihood of Testset
+    likelihood_t = calculateknn(testset.transpose(), samples, K)
+    likelihood_t = np.sum(np.log(likelihood_t))
+    print likelihood_t
+
     # plot result
     plt.plot(x[0, ...], distribution, label="KNN with K = {} and likelihood {}".format(K, likelihood))
     plt.title("KNN")
 
 plt.legend(fontsize="small", loc="best")
-
 plt.show()
