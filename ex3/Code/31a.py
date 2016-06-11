78dfb57e4c0abc396c43c1ef805d10cda511ee6f
david <davidgroemling@gmail.com>
2016-06-11 16:59:42 +0200

    Code for RMSE added

--- ex3/Code/31a.py
+++ ex3/Code/31a.py
@@ -12,8 +12,8 @@ def exprepeat(x_in, n):
 
     X_out = X_out.transpose()
 
-    print "X_out", X_out[:,0]
-    print "X_in", x_in.transpose()
+    #print "X_out", X_out[:,0]
+    #print "X_in", x_in.transpose()
     return X_out
 
 
@@ -27,6 +27,7 @@ def polyregress(x,y,degree):
 
 
 data = np.loadtxt("../dataSets/linRegData.txt")
+test = data[:,1]
 trainsize = 20
 
 x_train = np.array([data[0:trainsize,0]]).transpose()
@@ -37,28 +38,43 @@ data = data[data[:,0].argsort()]
 x_test = np.array([data[:,0]]).transpose()
 y_test = np.array([data[:,1]]).transpose()
 
+plt.close("all")
 plt.plot(x_train, y_train, 'o')
 plt.plot(x_test, y_test, 'x')
 
-for degree in range(21,22):
+rmse_test_t = np.array([[]])
+rmse_train_t = np.array([[]])
+for degree in range(0,21):
     w, X_train = polyregress(x_train, y_train, degree)
-    print "w: ", w
-    #X = np.ones([data.shape[0], 1])
-    #for i in range(0, degree):
-    #    v = np.array([X[:, i]]).transpose()
-    #    v = np.multiply(v, x_test)
-    #    X = np.concatenate((X, v), axis=1)
-    #X = X.transpose()
 
     X = exprepeat(x_test, degree)
 
+    ye_train = np.dot(w, X_train).transpose()
+    ye_test = np.dot(w, X).transpose()
 
-    y2 = np.dot(w, X).transpose()
-    y2_train = np.dot(w, X_train).transpose()
-    plt.plot(x_test, y2, label='degree {}'.format(degree))
-    plt.plot(x_train, y2_train, '--')
+    # Calculate RMSE of test data
+    rmse_test = np.power(np.subtract(y_test, ye_test),2)
+    rmse_test = np.array([[np.divide(np.sum(rmse_test), rmse_test.size)]])
+    rmse_test_t = np.concatenate((rmse_test_t, rmse_test), axis=1)
+    # and train data
+    rmse_train = np.power(np.subtract(y_train, ye_train),2)
+    rmse_train = np.array([[np.divide(np.sum(rmse_train), rmse_train.size)]])
+    rmse_train_t = np.concatenate((rmse_train_t, rmse_train), axis=1)
 
+    print "RMSE of testset with degree {}: {}".format(degree, rmse_test)
+    print "RMSE of train with degree {}: {}".format(degree, rmse_train)
+
+    # y2_train = np.dot(w, X_train).transpose()
+    plt.plot(x_test, ye_test, label='degree {}'.format(degree))
+    plt.plot(x_train, ye_train, '--')
 
 plt.legend()
+
+plt.figure()
+xplt = np.array([np.arange(0,21)]).transpose()
+plt.plot(xplt, rmse_test_t.transpose(), label='RMSE of test set')
+plt.plot(xplt, rmse_train_t.transpose(), label='RMSE of train set')
+plt.legend()
+
 plt.show()
 
