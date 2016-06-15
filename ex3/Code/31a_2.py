8368d06737908e256b37ee92545622cd5c7ef5f2
Rwilmes <rene.wilmes@stud.tu-darmstadt.de>
2016-06-15 17:34:18 +0200

    added renes version of 31a

---
+++ ex3/Code/31a_2.py
@@ -0,0 +1,62 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def get_plot_points(x, y):
+	points = {}
+	for i in range(0, len(x_test)):
+		points[x_test[i]]=ye_test[i][0]
+	x_sorted = []
+	y_sorted = []
+	for x in sorted(points.keys()):
+		x_sorted.append(x)
+		y_sorted.append(points[x])
+	return x_sorted, y_sorted
+
+def get_phi_polynomial(x, degree):
+	phi_temp = np.zeros((degree, len(x)))
+
+	for i in range(0, degree):
+		for j in range(0, len(x)):
+			l =  np.power(x[j], i)
+			phi_temp[degree-(i+1)][j] = l
+	return phi_temp
+
+
+def get_w(y, phi):
+	w = np.linalg.inv(np.dot(phi, phi.transpose()))
+	w = np.array([np.dot(np.dot(w, phi), y)])
+	return w
+
+
+# load data
+data = np.loadtxt("../dataSets/linRegData.txt")
+trainsize = 20
+dimension = 20
+
+x_test = data[trainsize:,0]
+x_train = data[0:trainsize,0]
+
+y_test = data[trainsize:,1]
+y_train = data[0:trainsize,1]
+
+# plot data points
+plt.plot(x_train, y_train, 'o')
+plt.plot(x_test, y_test, 'x')
+
+# calc curves and plot them
+for degree in range(0, dimension):
+	phi = get_phi_polynomial(x_train, degree)
+	w = get_w(y_train, phi)
+	X = get_phi_polynomial(x_test, degree)
+
+	# fit curve
+	ye_test = np.dot(w, X).transpose()
+
+	# sort points for linespoint plot
+	x_sorted, y_sorted = get_plot_points(x_test, ye_test)
+
+	# plot
+	plt.plot(x_sorted, y_sorted)
+
+plt.show()
+
