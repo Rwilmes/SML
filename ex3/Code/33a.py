ee3793eb4faa6d939f6532995c821cecf38ed79f
David <davidgroemling@gmail.com>
2016-06-15 19:27:48 +0200

    33a continued

--- ex3/Code/33a.py
+++ ex3/Code/33a.py
@@ -1,33 +1,57 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plotArray(X, c):
 
+def plotArray(X, c):
     for i in np.arange(0, X.shape[0]):
-        plt.plot(X[i,1], X[i,2], c)
+        plt.plot(X[i, 1], X[i, 2], c)
 
     return
 
-def normalize(X):
-    plotArray(X, 'bo')
 
+def normalize(X):
+    # Subtract mean
     m = np.array([np.mean(X, 0)])
     m = np.repeat(m, X.shape[0], 0)
     X = np.subtract(X, m)
-    plotArray(X, 'ro')
 
-    var = np.array([np.var(X, 0)])
-    var = np.repeat(var, X.shape[0], 0)
-    X = np.divide(X, var)
-    plotArray(X, 'go')
-    plt.show()
+    # Devide by standard deviation
+    dev = np.array([np.std(X, 0)])
+    X = np.divide(X, dev)
 
     return X
 
+
+def pca(X):
+    covar = np.cov(X, rowvar=False)
+    [eval, evec] = np.linalg.eig(covar)
+    return eval, evec
+
+
 data = np.loadtxt('../dataSets/iris.txt', delimiter=',')
 data = normalize(data)
+[eval, evec] = pca(data)
 
+totalVariance = np.sum(eval)
 
+componentsNeeded = 0
+explainedVariance = np.cumsum(eval)/np.sum(eval)
+print "Explained: ", explainedVariance
+i = 0
+while explainedVariance[i] < 0.95:
+    i += 1
 
+datap = np.dot(evec, data.T).T
+datap = datap[:, np.arange(0, 2)]
+plt.plot(datap[:, 0], datap[:, 1], '.')
+plt.plot(data[:, 0], data[:, 1], 'x')
 
-print data
+plt.show()
+
+print "Necessary components"
+print i
+print "Variance explained", explainedVariance / totalVariance
+print "Eigen values: "
+print eval
+print "Evec: "
+print evec
