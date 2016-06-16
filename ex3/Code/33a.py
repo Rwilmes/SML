4bdb06fe3f7586ec07b4953003ea378caa3ca3ef
david <davidgroemling@gmail.com>
2016-06-16 13:12:48 +0200

    3.3a-3.3c done, 3.3d started

--- ex3/Code/33a.py
+++ ex3/Code/33a.py
@@ -19,39 +19,74 @@ def normalize(X):
     dev = np.array([np.std(X, 0)])
     X = np.divide(X, dev)
 
-    return X
+    return X, m, dev
 
 
 def pca(X):
     covar = np.cov(X, rowvar=False)
-    [eval, evec] = np.linalg.eig(covar)
-    return eval, evec
+    [lambdas, evec] = np.linalg.eig(covar)
+    return lambdas, evec
 
 
+def unnormalize(X, m, dev):
+    # multiply with standard deviation
+    X = np.multiply(X, dev)
+
+    # add mean
+    X = np.add(X, m)
+
+    return X
+
+
+# Load data and split into features and classes
 data = np.loadtxt('../dataSets/iris.txt', delimiter=',')
-data = normalize(data)
+classes = data[:, 4]
+data = data[:, 0:4]
+
+# Separate samples of different classes
+c0 = np.equal(classes, 0)
+c1 = np.equal(classes, 1)
+c2 = np.equal(classes, 2)
+
+# Normalize data and perform PCA
+[data, m, dev] = normalize(data)
 [eval, evec] = pca(data)
 
-totalVariance = np.sum(eval)
-
-componentsNeeded = 0
-explainedVariance = np.cumsum(eval)/np.sum(eval)
+# Calculate explained variance
+explainedVariance = np.cumsum(eval) / np.sum(eval)
 print "Explained: ", explainedVariance
-i = 0
-while explainedVariance[i] < 0.95:
-    i += 1
 
-datap = np.dot(evec, data.T).T
-datap = datap[:, np.arange(0, 2)]
-plt.plot(datap[:, 0], datap[:, 1], '.')
-plt.plot(data[:, 0], data[:, 1], 'x')
+# Calculate number of features necessary to explain at least 95% variance
+componentsNeeded = 0
+while explainedVariance[componentsNeeded] < 0.95:
+    componentsNeeded += 1
 
+# Project data onto principal components
+datap = np.dot(evec.T, data.T).T
+
+# Plot result
+plt.plot(datap[c0, 0], datap[c0, 1], 'o', label='Setosa')
+plt.plot(datap[c1, 0], datap[c1, 1], 'o', label='Versicolour')
+plt.plot(datap[c2, 0], datap[c2, 1], 'o', label='Virginica')
+plt.xlabel("Eigenvactor 1")
+plt.ylabel('Eigenvecotr 2')
+plt.legend(loc='best')
 plt.show()
 
-print "Necessary components"
-print i
-print "Variance explained", explainedVariance / totalVariance
-print "Eigen values: "
-print eval
-print "Evec: "
-print evec
+for i in np.arange(1, 5):
+    # Set all unused principal components to 0
+    data_restored = np.copy(datap)
+    data_restored[:, i:] = 0
+
+    # Project features back to original feature space
+    data_restored = np.linalg.solve(evec.T, data_restored.T).T
+    #data_restored = unnormalize(data_restored, m, dev)
+
+    # Calculate RMSE
+    #e = np.subtract(unnormalize(data, m, dev), data_restored)
+    e = np.subtract(data, data_restored)
+    e **= 2
+    e = np.sum(e, axis=1)
+    e = np.divide(np.sum(e, axis=0), e.size)
+    e = np.sqrt(e)
+    print e
