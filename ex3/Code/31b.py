6f9a6b9393cceb5d3a4591b5d17950912d2cd4d7
Rwilmes <rene.wilmes@stud.tu-darmstadt.de>
2016-06-16 18:33:32 +0200

    updated 31b

--- ex3/Code/31b.py
+++ ex3/Code/31b.py
@@ -15,18 +15,19 @@ def normalize(ys, x):
 
 	return ys
 
-space = np.linspace(0, 2, num=20)
+def get_gaussians(space, x, var):
+	ys = []
+	for i in space:
+		mu = i
+		sigma = math.sqrt(var)	
+		y = mlab.normpdf(x, mu, sigma)
+		ys.append(y)
+	return ys
 
-ys = []
+space = np.linspace(0, 2, num=20)
 x = np.linspace(0, 2, 1000)
 
-for i in space:
-	mu = i
-	variance = 0.02
-	sigma = math.sqrt(variance)	
-	y = mlab.normpdf(x, mu, sigma)
-	ys.append(y)
-
+ys = get_gaussians(space, x, 0.02)
 ys = normalize(ys, x)
 
 for y in ys:
