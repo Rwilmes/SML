4aaf10ce89003c7f35853eb5dd0675f79a2485d0
David <davidgroemling@gmail.com>
2016-07-08 09:08:46 +0200

    Matlab files for perceptron added

--- ex4/dataSets/train_nn.m
+++ ex4/dataSets/train_nn.m
@@ -1,15 +1,32 @@
 clear variables;
 
 X = load('mnist_small_train_in.txt');
+X = [ones(size(X,1) ,1), X];
 y = load('mnist_small_train_out.txt');
+X_test = load('mnist_small_test_in.txt');
+y_test = load('mnist_small_test_out.txt');
 
-num_h = 200;
+num_h = 5;
 
-W1 = ones(num_h, size(X, 1));
-W2 = ones(size(y,1), num_h);
+[Wx, Wy, MSE] = trainMLP(size(X,2), 10, 1, 1, 0.001, X', y', 1000, 0.1);
 
-z = sum(W1*X, 2);
-yhat = sum(W2*z, 2);
+semilogy(MSE);
+W1 = 0.1*ones(num_h, size(X, 1)+1);
+W2 = 0.1*ones(size(y,1), num_h+1);
 
-error2 = (y-yhat);
-error1 = error2' * W2;
+for i=1:10
+    z = W1*X;
+    yhat = sum(W2*z, 2);
+    
+    error2 = (y-yhat);
+    error1 = W2' * error2;
+    
+    dW2 = error2 * z';
+    dW1 = W1.*repmat(error1, 1, size(W1,2));
+    
+    W2 = W2 - 0.1*dW2;
+    W1 = W1 - 0.1*dW1;
+    
+    e = sqrt(sum(error2.^2)/length(error2));
+    disp(e);
+end
